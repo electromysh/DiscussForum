0cb92d860655d4ab79abe1fd45a78e3544947dd9
Darina Kovalskaya <57908654+holtzdari@users.noreply.github.com>
2022-07-13 11:32:02 +0300

    87. Added plus btn

---
+++ lib/discuss_web/controllers/topic_controller.ex
@@ -0,0 +1,30 @@
+defmodule DiscussWeb.TopicController  do
+  use DiscussWeb, :controller
+
+  alias Discuss.Discussions.Topic, as: Topic
+
+  def index(conn, _params) do
+    topics = Discuss.Repo.all(Topic)
+    render conn, "index.html", topics: topics
+  end
+
+  def new(conn, _params) do
+    changeset = Topic.changeset(%Topic{}, %{})
+
+    render conn, "new.html", changeset: changeset
+  end
+
+  def create(conn, %{"topic" => topic}) do
+    changeset = Topic.changeset(%Topic{}, topic)
+
+    case Discuss.Repo.insert(changeset) do
+      {:ok, post} ->
+        conn
+        |> put_flash(:info, "Topic Created")
+        |> redirect(to: Routes.topic_path(conn, :index))
+      {:error, changeset} ->
+        render conn, "new.html", changeset: changeset
+    end
+  end
+
+end
